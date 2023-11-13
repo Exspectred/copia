50cb6112e436eed55bfd89057e3816afb3d163d8
fedecoronel <51930636+Exspectred@users.noreply.github.com>
2023-11-12 22:49:10 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
-const prevBtns = document.querySelectorAll('.btn-pre')
-const nextBtns = document.querySelectorAll(('.btn-next'))
-const progress = document.getElementById('progress')
-const formSteps = document.querySelectorAll('.form-step')
-const progressSteps = document.querySelectorAll('.progress-step')
+const prevBtns = document.querySelectorAll('.btn-pre') // VARIABLE PARA SELECCIONAR LOS BOTONES DE ANTERIOR
+const nextBtns = document.querySelectorAll(('.btn-next')) // VARIABLE PARA SELECCIONAR LOS BOTONES DE SIGUIENTE
+const formSteps = document.querySelectorAll('.form-step') // VARIABLE PARA SELECCIONAR MIS 4 FORMS
+const progressSteps = document.querySelectorAll('.progress-step') // VARIABLE PARA SELECCIONAR MIS 4 CIRCULOS DEL FORM
 
 let formStepsNum = 0;
 
+// CLICK BOTON SIGUIENTE = formStepsNum + 1, actualiza updateFormSteps y updateProgressbar
 nextBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         formStepsNum++;
@@ -14,6 +14,7 @@ nextBtns.forEach(btn => {
     })
 })
 
+// CLICK BOTON ANTERIOR = formSteps - 1, actualiza updateFormSteps y updateProgressbar
 prevBtns.forEach(btn => {
     btn.addEventListener('click', () => {
         formStepsNum--;
@@ -22,6 +23,7 @@ prevBtns.forEach(btn => {
     })
 })
 
+// Remueve la class ACTIVE del form que queremos dejar y se lo aplica al actual
 function updateFormSteps(){
     formSteps.forEach(formStep =>{
         formStep.classList.contains('active')&&
@@ -30,6 +32,7 @@ function updateFormSteps(){
     formSteps[formStepsNum].classList.add('active')
 }
 
+// Agrega o quita el color negro de cada círculo en mi formulario
 function updateProgressbar(){
     progressSteps.forEach((progressStep, idx) => {
         if (idx  < formStepsNum + 1) {
